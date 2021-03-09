b8b81fbd9b5a0f0042852f6201b5ce8b8ca977b7
mervenur teke <313958@ogr.ktu.edu.tr>
2021-03-10 02:50:52 +0300

    17 Şubat ValidationFluent

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constanst;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -21,6 +23,8 @@ namespace Business.Concrete
 
         public IResult Add(Rental rental)
         {
+            ValidationTool.Validate(new RentalValidator(), rental);
+
             _rentalDal.Add(rental);
             return new SuccessResult(Messages.RentalAdded);
         }
